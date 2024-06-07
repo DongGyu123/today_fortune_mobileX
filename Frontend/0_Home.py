5ebacf55ffd762229ca0cfe8aa95222eac866a34
DongGyu123 <110375337+DongGyu123@users.noreply.github.com>
2024-06-07 16:52:21 +0900

    Add files via upload

---
+++ Frontend/0_Home.py
@@ -0,0 +1,18 @@
+import i18n
+import streamlit as st
+
+from utils.init import init_once
+
+
+if __name__ == '__main__':
+    # Init
+    init_once()
+
+    # Show title
+    st.title(i18n.t('오늘의 운세'))
+
+    # Show page description
+    st.write(i18n.t('이름, 성별, 생년월일, 태어난 시각을 선택하면 오늘의 운세를 알려드립니다!'))
+
+    # Show github link
+    st.write(f'* Github: {i18n.t("https://github.com/DongGyu123/fortune")}')
